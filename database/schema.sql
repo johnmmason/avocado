c567e139d4b93e9fe941bae2e3eb32897eec670b
johnmmason <jmmason@utexas.edu>
2021-04-27 08:48:30 -0500

    init database schema

--- database/schema.sql
+++ database/schema.sql
@@ -3,5 +3,20 @@
 \c avocado;
 
 CREATE TABLE avocado(
-    id SERIAL
+    id SERIAL PRIMARY KEY,
+    week_id INTEGER,
+    week DATE,
+    price REAL,
+    volume REAL,
+    total_4046 REAL,
+    total_4225 REAL,
+    total_4770 REAL,
+    category VARCHAR(20),
+    year INTEGER,
+    region VARCHAR(20)
 );
+
+COPY avocado(week_id, week, price, volume, total_4046, total_4225, total_4770, category, year, region)
+FROM '/avocado.csv'
+DELIMITER ','
+CSV HEADER;
