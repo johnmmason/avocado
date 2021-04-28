8c270538d088a8c2ce36447bbe5417aca93b88bd
johnmmason <jmmason@utexas.edu>
2021-04-28 15:01:56 -0500

    fixed container launch and networking issues

--- src/worker.py
+++ src/worker.py
@@ -5,6 +5,6 @@ import time
 def execute_job(jid):
     update_job(jid, 'start', {'job_type':'insert', 'status':'in_progress'})
     time.sleep(15)
-    update_job(jid, 'end', {'job_type':'insert', 'status':'complete',)
+    update_job(jid, 'end', {'job_type':'insert', 'status':'complete'})
 
 execute_job()
